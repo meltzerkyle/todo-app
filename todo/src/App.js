0efba0928331389fc16d25431cf0ac7d4a3451b4
meltzerkyle <meltzerkyle@gmail.com>
2021-05-05 08:27:16 -0500

    WIP on basic todo components and app structure

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import ToDo from './reducers/Reducer.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <h4>Kyle's To-Do App</h4>
       </header>
+      <div>
+        <ToDo />
+      </div>
     </div>
   );
 }
